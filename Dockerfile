c2c1506e7fe3442fb0a93e677e356ff7c9598c99
varshithsai96 <109300077+varshithsai96@users.noreply.github.com>
2023-04-03 12:37:47 -0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+
+FROM openjdk-11
+
+RUN apt update && apt install -y maven 3.6.3
+
+
+WORKDIR /Users/varshithsaipeddi/desktop/newproj
---
+++ Jenkinsfile.groovy
@@ -0,0 +1,81 @@
+pipeline {
+    agent any
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10'))
+        disableConcurrentBuilds()
+        timeout(time: 1, unit: 'HOURS')
+        timestamps()
+    }
+
+    tools {
+        jdk 'openjdk-11'
+        maven 'maven 3.6.3'
+        dockerTool 'docker-latest'
+    }
+
+    environment {
+        POM_VERSION = getVersion()
+        JAR_NAME = getJarName()
+        AWS_ECR_REGION = 'us-west-1'
+        AWS_ECS_SERVICE = 'ch-dev-user-api-service'
+        AWS_ECS_TASK_DEFINITION = 'ch-dev-user-api-taskdefinition'
+        AWS_ECS_COMPATIBILITY = 'FARGATE'
+        AWS_ECS_NETWORK_MODE = 'awsvpc'
+        AWS_ECS_CPU = '256'
+        AWS_ECS_MEMORY = '512'
+        AWS_ECS_CLUSTER = 'ch-dev'
+        AWS_ECS_TASK_DEFINITION_PATH = './ecs/container-definition-update-image.json'
+    }
+
+    stage('Build & Test') {
+            steps {
+                withMaven(options: [artifactsPublisher(), mavenLinkerPublisher(), dependenciesFingerprintPublisher(disabled: true), jacocoPublisher(disabled: true), junitPublisher(disabled: true)])
+                 {
+                sh "mvn -B -U clean package"
+                }
+            }
+        }
+}        
+"""
+    stage('Build Docker Image') {
+                steps {
+                    withCredentials([string(credentialsId: '5f567787-4fc1-47c4-8194-d2885f1d1d08', variable: 'AWS_ECR_URL')]) {
+                    script {
+                        docker.build("${AWS_ECR_URL}:${POM_VERSION}", "--build-arg JAR_FILE=${JAR_NAME} .")
+                    }
+                }
+            }
+        }
+
+        stage('Push Image to ECR') {
+        steps {
+                    withCredentials([string(credentialsId: '5f567787-4fc1-47c4-8194-d2885f1d1d08', variable: 'AWS_ECR_URL')]) {
+                    withAWS(region: "${AWS_ECR_REGION}", credentials: 'aws') {
+                    script {
+                        def login = ecrLogin()
+                        sh('#!/bin/sh -e\n' + "${login}") // hide logging
+                        docker.image("${AWS_ECR_URL}:${POM_VERSION}").push()
+                        }
+                    }
+                }
+            }
+        }
+
+    stage('Deploy in ECS') {
+        steps {
+                    withCredentials([string(credentialsId: '5f567787-4fc1-47c4-8194-d2885f1d1d08', variable: 'AWS_ECS_EXECUTION_ROL')]) {
+                    script {
+                        updateContainerDefinitionJsonWithImageVersion()
+                        sh("/usr/local/bin/aws ecs register-task-definition --region ${AWS_ECR_REGION} --family ${AWS_ECS_TASK_DEFINITION} --execution-role-arn ${AWS_ECS_EXECUTION_ROL} --requires-compatibilities ${AWS_ECS_COMPATIBILITY} --network-mode ${AWS_ECS_NETWORK_MODE} --cpu ${AWS_ECS_CPU} --memory ${AWS_ECS_MEMORY} --container-definitions file://${AWS_ECS_TASK_DEFINITION_PATH}")
+                        def taskRevision = sh(script: "/usr/local/bin/aws ecs describe-task-definition --task-definition ${AWS_ECS_TASK_DEFINITION} | egrep \"revision\" | tr \"/\" \" \" | awk '{print \$2}' | sed 's/\"\$//'", returnStdout: true)
+                        sh("/usr/local/bin/aws ecs update-service --cluster ${AWS_ECS_CLUSTER} --service ${AWS_ECS_SERVICE} --task-definition ${AWS_ECS_TASK_DEFINITION}:${taskRevision}")
+                    }
+            }
+        }
+            
+    }
+    
+
+    post {}
+}"""
